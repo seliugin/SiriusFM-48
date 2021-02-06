455b357b76bf8dea54a4319f3bb67a1741ec10b3
Sirius <none@none.none>
2021-02-06 18:00:46 +0300

    add IRProvider

---
+++ IRProviderConst.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "IRProvider.h"
+
+namespace SiriusFM
+{
+	template <>
+	class IRProvider<IRMode::Const>:
+	{
+		private:
+			double m_IRs[CcyE::N];
+		public:
+			IRProvider(char const* a_file);
+			double r(CcyE a_ccy, double t) const
+			{
+				return m_IRs[int(a_ccy)];
+			}
+	}
+}
+
