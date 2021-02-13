70d699d583356a854f29c42a5a6fabdf1ce502fa
seliugin <mike_super1@mail.ru>
2021-02-13 10:08:43 +0300

    upd Time, IRP

---
+++ Time.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace SiriusFM
+{
+	constexpr int SEC_IN_MIN 		= 60;
+	constexpr int SEC_IN_DAYS 		= 86400;
+	constexpr double AVG_DAYS_IN_YEAR 	= 365.25;
+	constexpr double AVG_SEC_IN_YEAR 	= AVG_DAYS_IN_YEAR*SEC_IN_DAYS;
+	constexpr double EPOCH_BEGIN 		= 1970.0;
+
+	inline double YearFrac(time_t a_t)
+	{
+		return EPOCH_BEGIN + double(a_t) / AVG_SEC_IN_YEARS;
+	}
+
+	inline double YearFracInt(time_t a_t)
+	{                                                  
+		return double(a_t) / AVG_SEC_IN_YEARS;   
+	}
+}
